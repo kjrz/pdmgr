6108409eed658a164b2a4b77899c29b18c3c16a0
kjrz <kuba.jarz@gmail.com>
2015-03-17 17:03:55 +0100

    Fix indentation in 201 SQL

--- instamine/sql/triads/201.sql
+++ instamine/sql/triads/201.sql
@@ -1,6 +1,6 @@
-           SELECT ab.follower_id AS a_id,
-                  ba.follower_id AS b_id,
-                  ca.follower_id AS c_id
+SELECT ab.follower_id AS a_id,
+       ba.follower_id AS b_id,
+       ca.follower_id AS c_id
            FROM following AS ab
            JOIN following AS ba ON ab.followee_id = ba.follower_id
                                AND ab.follower_id = ba.followee_id
@@ -11,11 +11,11 @@ LEFT OUTER JOIN following AS bc ON bc.follower_id = ba.follower_id
                                AND bc.followee_id = ca.follower_id
 LEFT OUTER JOIN following AS cb ON cb.follower_id = ca.follower_id
                                AND cb.followee_id = ba.follower_id
-           WHERE bc.follower_id IS NULL
-             AND bc.followee_id IS NULL
-             AND cb.follower_id IS NULL
-             AND cb.followee_id IS NULL
-             AND (   (    ac.follower_id > ba.follower_id
-                  AND ba.follower_id > ca.follower_id)
-                  OR (    ac.follower_id < ba.follower_id
-                  AND ba.follower_id > ca.follower_id))
+WHERE bc.follower_id IS NULL
+  AND bc.followee_id IS NULL
+  AND cb.follower_id IS NULL
+  AND cb.followee_id IS NULL
+  AND (   (    ac.follower_id > ba.follower_id
+       AND ba.follower_id > ca.follower_id)
+       OR (    ac.follower_id < ba.follower_id
+       AND ba.follower_id > ca.follower_id))
