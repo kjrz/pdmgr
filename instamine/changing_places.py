082247797c5a38be6fc95e34b49473b07eaaab30
kjrz <kuba.jarz@gmail.com>
2015-04-18 20:26:10 +0200

    Add changing places

---
+++ instamine/changing_places.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+
+DB_ORIGIN = '../data/number_ten (2nd user at etbastpurgato).db'
+DB_COPY = '../data/number_ten (new followings copy).db'
+
+try:
+    os.remove(DB_COPY)
+except OSError:
+    pass
+
+origin_conn = sqlite3.connect(DB_ORIGIN)
+origin = origin_conn.cursor()
+copy_conn = sqlite3.connect(DB_COPY)
+copy = copy_conn.cursor()
+
+origin.execute('SELECT COUNT(*) FROM following WHERE first_seen > (SELECT fin FROM effort WHERE id = 1);')
+print origin.fetchone()[0], 'new triads'
+
+copy.execute('CREATE TABLE following ('
+             'follower_id INTEGER,'
+             'followee_id INTEGER,'
+             'PRIMARY KEY (follower_id, followee_id))')
+
+origin.execute('SELECT follower_id, followee_id FROM following '
+               'WHERE first_seen > (SELECT fin FROM effort WHERE id = 1)')
+for res in origin:
+    copy.execute('INSERT INTO following (follower_id, followee_id) VALUES (?, ?)', res)
+
+copy_conn.commit()
